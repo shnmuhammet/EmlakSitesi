e76fc0302b728fff136ad255be2e64f0c0795d22
Shnmuhammet <shnn.muhammetemin@gmail.com>
2024-08-05 14:56:43 +0300

    Admin Tarafı başlandı

---
+++ RealEstate_Dapper_UI/ViewComponents/AdminLayout/_AdminLayoutHead.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstate_Dapper_UI.ViewComponents._AdminLayout
+{
+    public class _AdminLayoutHead:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { 
+            return View(); 
+        }
+    }
+}
