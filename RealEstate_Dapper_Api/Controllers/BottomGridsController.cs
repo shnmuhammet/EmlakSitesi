1755e49f034fdb76b47710f290692d1d10c82d35
Shnmuhammet <shnn.muhammetemin@gmail.com>
2024-08-09 21:06:30 +0300

    BottomGrid Api Ve Consume41

--- RealEstate_Dapper_Api/Controllers/BottomGridsController.cs
+++ RealEstate_Dapper_Api/Controllers/BottomGridsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
+using RealEstate_Dapper_Api.Dtos.BottomGridDtos;
 using RealEstate_Dapper_Api.Repositories.BottomGridRepositories;
 
 namespace RealEstate_Dapper_Api.Controllers
@@ -20,6 +22,30 @@ namespace RealEstate_Dapper_Api.Controllers
         {
             var value=await _bottomGridRepository.GetAllBottomGridAsync();
             return Ok(value) ;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateBottomGrid(CreateBottomGridDto createBottomGridDto)
+        {
+            _bottomGridRepository.CreateBottomGrid(createBottomGridDto);
+            return Ok("Veri başarıyla eklendi");
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBottomGrid(int id)
+        {
+            _bottomGridRepository.DeleteBottomGrid(id);
+            return Ok("Veri başarıyla silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpadeteBottomGrid(UpdateBottomGridDto updateBottomGridDto)
+        {
+            _bottomGridRepository.UpdateBottomGrid(updateBottomGridDto);
+            return Ok("Veri Güncelledni");
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBottomGrid(int id)
+        {
+            var value = await _bottomGridRepository.GetBottomGrid(id);
+            return Ok(value);
         }
     }
 }
